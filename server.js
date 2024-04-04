73406b5e56a0b7dc6647ce23463e204803fbf770
Sang <phunganhsang123@gmail.com>
2024-04-05 01:27:46 +0700

    server to send location

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
-// server.js
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Socket.IO connection event
-io.on('connection', (socket) => {
-    console.log('A new device connected');
-
-    // Event for receiving device position updates
-    socket.on('positionUpdate', (data) => {
-        console.log('Position update received from device:', data);
-        // Broadcast the position update to all connected devices except the sender
-        socket.broadcast.emit('positionUpdate', data);
-    });
-
-    // Event for when a device disconnects
-    socket.on('disconnect', () => {
-        console.log('Device disconnected');
-    });
+const server = require("http").Server(app)
+const socketIO = require("socket.io")(server)
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on('location', (location) => {
+    console.log(location);
+    socket.emit("location",location)
+  });
+  
+  socket.on('disconnect', () => {
+    socket.disconnect()
+    console.log('🔥: A user disconnected');
+  });
 });
